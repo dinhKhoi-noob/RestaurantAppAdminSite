0e676407ea370fdf7c1a733d9977cfc16f5a5d05
dinhKhoi-noob <khoip1305@gmail.com>
2022-01-07 16:59:37 +0700

    feat(dashboard): Completed building dashboard UI

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DashboardCard from './DashboardCard';
 import LineChart from './LineChart';
 import PieChart from './PieChart';
+import LatestSection from './LatestSection';
 import { MdLocalShipping } from 'react-icons/md';
 import { FaUserTag } from 'react-icons/fa';
 import { RiMoneyDollarCircleFill } from 'react-icons/ri';
@@ -44,78 +45,42 @@ const Dashboard = () => {
                     mainColor="yellow"
                 />
             </div>
-                <div className="flex-row dashboard-chart__container">
-                    <div className="dashboard-chart__wrapper-line">
-                        <div className="dashboard-chart__title">
-                            Total order and profit
-                        </div>
-                        <div className="dashboard-chart__dropdown-section m-r-1rem">
-                            <span>Order by: </span>
-                            <select className="dashboard-chart__dropdown-selector">
-                                <option value="this_week">This week</option>
-                                <option value="this_month">This month</option>
-                                <option value="this_year">This year</option>
-                                <option value="2021">2021</option>
-                                <option value="2020">2020</option>
-                                <option value="2019">2019</option>
-                            </select>
-                        </div>
-                        <LineChart/>
+            <div className="flex-row dashboard-chart__container">
+                <div className="dashboard-chart__wrapper-line">
+                    <div className="dashboard-chart__title">
+                        Total order and profit
                     </div>
-                    <div className="dashboard-chart__wrapper-pie">
-                        <div className="dashboard-chart__title">
-                            Total ordered dishes by category
-                        </div>
-                        <div className="dashboard-chart__dropdown-section">
-                            <span>Order by: </span>
-                            <select className="dashboard-chart__dropdown-selector">
-                                <option value="this_week">This week</option>
-                                <option value="this_month">This month</option>
-                                <option value="this_year">This year</option>
-                                <option value="2021">2021</option>
-                                <option value="2020">2020</option>
-                                <option value="2019">2019</option>
-                            </select>
-                        </div>
-                        <PieChart/>
+                    <div className="dashboard-chart__dropdown-section m-r-1rem">
+                        <span>Order by: </span>
+                        <select className="dashboard-chart__dropdown-selector">
+                            <option value="this_week">This week</option>
+                            <option value="this_month">This month</option>
+                            <option value="this_year">This year</option>
+                            <option value="2021">2021</option>
+                            <option value="2020">2020</option>
+                            <option value="2019">2019</option>
+                        </select>
                     </div>
+                    <LineChart/>
                 </div>
-                <div className="flex-row dashboard-chart__container">
-                    <div className="dashboard-chart__wrapper-line">
-                        <div className="dashboard-chart__title">
-                            Total order and profit
-                        </div>
-                        <div className="dashboard-chart__dropdown-section m-r-1rem">
-                            <span>Order by: </span>
-                            <select className="dashboard-chart__dropdown-selector">
-                                <option value="this_week">This week</option>
-                                <option value="this_month">This month</option>
-                                <option value="this_year">This year</option>
-                                <option value="2021">2021</option>
-                                <option value="2020">2020</option>
-                                <option value="2019">2019</option>
-                            </select>
-                        </div>
-                        <LineChart/>
+                <div className="dashboard-chart__wrapper-pie">
+                    <div className="dashboard-chart__title">
+                        Total ordered dishes by category
                     </div>
-                    <div className="dashboard-chart__wrapper-pie">
-                        <div className="dashboard-chart__title">
-                            Total ordered dishes by category
-                        </div>
-                        <div className="dashboard-chart__dropdown-section">
-                            <span>Order by: </span>
-                            <select className="dashboard-chart__dropdown-selector">
-                                <option value="this_week">This week</option>
-                                <option value="this_month">This month</option>
-                                <option value="this_year">This year</option>
-                                <option value="2021">2021</option>
-                                <option value="2020">2020</option>
-                                <option value="2019">2019</option>
-                            </select>
-                        </div>
-                        <PieChart/>
+                    <div className="dashboard-chart__dropdown-section">
+                        <select className="dashboard-chart__dropdown-selector">
+                            <option value="this_week">This week</option>
+                            <option value="this_month">This month</option>
+                            <option value="this_year">This year</option>
+                            <option value="2021">2021</option>
+                            <option value="2020">2020</option>
+                            <option value="2019">2019</option>
+                        </select>
                     </div>
+                    <PieChart/>
                 </div>
+            </div>
+            <LatestSection/>
         </div>
     )
 }
